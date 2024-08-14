bd120baa63dcdace2594c1322cf66d9127118887
iraboldyrieva <iraboldyrieva@gmail.com>
2024-08-14 15:54:29 +0300

    Add eslint and prettier

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
@@ -7,5 +7,5 @@ const port = 3000;
 // middleware
 app.use(bodyParser.json());
 app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}`);
+    console.log(`Server listening on http://localhost:${port}`);
 });
